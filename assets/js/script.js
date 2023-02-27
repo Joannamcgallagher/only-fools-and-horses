c35a56938bbbdcdccdfad9eb7a103485a14e637b
Joannamcgallagher <joannamcgallagher@gmail.com>
2023-02-27 21:06:18 +0000

    Add code to script.js for form to allow users to enter names

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,30 +24,58 @@ function createUserDetailsPage()
     document.getElementById("intro-paragraph").textContent = "";
 
     //change the main image and make set the height smaller
-    document.getElementById("game-area-background").style.opacity = "0.7";
+    document.getElementById("game-area-background").style.background = "url('assets/images/banner.png') no-repeat center center";
+    document.getElementById("game-area-background").style.height = "275px"
+    //remove the buttons
+    // let button = document.getElementById("play-now");
+    // button.remove();
 
-    //create the label and input fields & add them to the DOM tree
-    let userArray = ["userLabel", "inputUser"];
-    userArray.userLabel = document.createElement("label");
-    //set attributes
-    userArray.userLabel.setAttribute("for", "user-name-label");
-    userArray.userLabel.setAttribute("id", "user-name-label");
-    userArray.userLabel.style.opacity = "1";
-    userArray.userLabel.textContent = "Enter your name here: ";
+    let buttons = document.getElementsByTagName("button");
+    console.log(buttons);
+    for (let i = 0; i < buttons.length; i++)
+    {
+        buttons[i].remove();
+    }
+    console.log(buttons); //instructions button still showing when button clicked
+    let instrButton = document.getElementById("instructions");
+    instrButton.remove();
+    
+    //create a div to hold the label & input elements and add to body
+    let userDetails = document.createElement("div");
+    userDetails.setAttribute("id", "quiz-container");
+    userDetails.style.height = "275px"
+    userDetails.style.width = "60%";
+    userDetails.style.boxShadow = "20px yellow";
+    userDetails.style.border = "4px solid darkgoldenrod";
+    userDetails.style.borderRadius = "20px";
+    userDetails.style.marginLeft = "auto";
+    userDetails.style.marginRight = "auto";
+    document.body.appendChild(userDetails);
 
-    userArray.inputUser = document.createElement("input");
-    userArray.inputUser.setAttribute("id", "user-name");
-    userArray.inputUser.setAttribute("type", "text");
+    //create a form and add label & input 
+    let userForm = document.createElement("form");
+    userForm.setAttribute("id", "user-input");
+    userForm.style.textAlign = "center";
+    document.getElementById("quiz-container").appendChild(userForm);
+    userForm.style.display = "grid";
 
-    document.getElementById("game-area").appendChild(userArray.userLabel);
-  
+    let label = document.createElement("label");
+    label.setAttribute("for", "user-name");
+    label.innerHTML = "Enter your name here : ";
+    label.style.marginTop = "50px";
+    userForm.appendChild(label);
 
-    //change the text on the button
-    let submitButton = document.getElementById("play-now");
-    submitButton.textContent = "Let's go!!";
-}
+    let input = document.createElement("input");
+    input.setAttribute("name", "user-name");
+    input.setAttribute("id", "user-name");
+    input.setAttribute("type", "text");
+    userForm.appendChild(input);
 
-function mySimpleFunction(event)
-{
-    console.log("you clicked play-now");
+    //create a button
+    let submitButton = document.createElement("button");
+    submitButton.setAttribute("id", "lets-go");
+    submitButton.textContent = "Let's Go!!";
+    userForm.appendChild(submitButton);    
+    
+    
 }
